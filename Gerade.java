6b1c12050e5dfbb794e06fac2035f44f931fa914
Arouraios <lao_shu@gmx.de>
2020-12-22 23:08:29 +0100

    d) fertig

--- Gerade.java
+++ Gerade.java
@@ -1,31 +1,104 @@
+import java.math.*;
+
 public class Gerade {
-	private final p1, p2;
-	
+	private final Punkt p1, p2;
+
 	public Gerade(Punkt p1, Punkt p2) {
-		if(p1.equals(p2)) {
+		if (p1.equals(p2)) {
 			System.out.println("Die angegebenen Punkte dürfen nicht gleich sein!");
 			this.p1 = null;
 			this.p2 = null;
 		} else {
-			if(p1.getX().compareTo(p2.getX()) < 0) {
-				this.p1=p1;
-				this.p2=p2;
-			} else if(p1.getX().compareTo(p2.getX()) > 0) {
-				this.p1=p2;
-				this.p2=p1;
-			} else if(p1.getX().equals(p2.getX())) {
-				if(p1.getY().compareTo(p2.getY())<0) {
-					this.p1=p1;
-					this.p2=p2;
-				} else if(p1.getY().compareTo(p2.getY())>0) {
-					this.p1=p2;
-					this.p2=p1;
+			if (p1.getX().compareTo(p2.getX()) < 0) {
+				this.p1 = p1;
+				this.p2 = p2;
+			} else if (p1.getX().compareTo(p2.getX()) > 0) {
+				this.p1 = p2;
+				this.p2 = p1;
+			} else if (p1.getX().equals(p2.getX())) {
+				if (p1.getY().compareTo(p2.getY()) < 0) {
+					this.p1 = p1;
+					this.p2 = p2;
+				} else if (p1.getY().compareTo(p2.getY()) > 0) {
+					this.p1 = p2;
+					this.p2 = p1;
+				} else {
+					this.p1 = null;
+					this.p2 = null;
+					System.out.println(
+							"Diese Stelle im Code soll nicht erreicht werden. Wenn doch ist was schief gelaufen :/");
 				}
-			} else { System.out.println("Diese Stelle im Code soll nicht erreicht werden. Wenn doch ist was schief gelaufen :/");}
+			} else {
+				this.p1 = null;
+				this.p2 = null;
+				System.out.println(
+						"Diese Stelle im Code soll nicht erreicht werden. Wenn doch ist was schief gelaufen :/");
+			}
 		}
 	}
-	
+
 	public String toString() {
-		System.out.println("Gerade durch "+ p1.toString() + " und " + p2.toString());
+		return ("Gerade durch " + p1.toString() + " und " + p2.toString());
 	}
+
+	/*
+	 * Wenn p0 zwischen p1 und p2 liegt, dann ist der Abstand zu x positiv und
+	 * kleiner als der Abstand zwischen p1 und p2. Außerdem gilt: Wenn eine gerade
+	 * durch p1 und p0 die selbe Steigung hat wie die gerade durch p1 und p2, so
+	 * sind p0 und p2 auf der selben gerade.
+	 */
+	public/* !!!private */ boolean zwischenp1p2(Punkt p0) {
+		if(p0.equals(p1)) return true;
+		if(p0.equals(p2)) return true;
+		BigDecimal dx = p1.getX().subtract(p0.getX());
+		BigDecimal dy = p1.getY().subtract(p0.getY());
+		BigDecimal gDx = p2.getX().subtract(p1.getX());
+		BigDecimal gDy = p2.getY().subtract(p1.getY());
+		if (dx.compareTo(BigDecimal.ZERO) >= 0 && dy.compareTo(BigDecimal.ZERO) >= 0 && dx.compareTo(gDx) <= 0
+				&& dy.compareTo(gDy) <= 0 && BigDecimalUtility.equalValues(dx.divide(dy), gDx.divide(gDy)))
+			return true;
+		return false;
+	}
+
+	public/* !!!private */ boolean vorp1(Punkt p0) {
+		if (zwischenp1p2(p0))
+			return false;
+		BigDecimal dx = p1.getX().subtract(p0.getX());
+		BigDecimal dy = p1.getY().subtract(p0.getY());
+		BigDecimal gDx = p2.getX().subtract(p1.getX());
+		BigDecimal gDy = p2.getY().subtract(p1.getY());
+		if (dx.compareTo(BigDecimal.ZERO) > 0 && dy.compareTo(BigDecimal.ZERO) > 0
+				&& BigDecimalUtility.equalValues(dx.divide(dy), gDx.divide(gDy)))
+			return true;
+		return false;
+	}
+
+	public/* !!!private */ boolean hinterp2(Punkt p0) {
+		if (zwischenp1p2(p0))
+			return false;
+		if (vorp1(p0))
+			return false;
+		BigDecimal dx = p1.getX().subtract(p0.getX());
+		BigDecimal dy = p1.getY().subtract(p0.getY());
+		BigDecimal gDx = p2.getX().subtract(p1.getX());
+		BigDecimal gDy = p2.getY().subtract(p1.getY());
+		if (!BigDecimalUtility.equalValues(dx.divide(dy), gDx.divide(gDy)))
+			return false;
+		
+		return true;
+	}
+
+	public boolean enthaelt(Punkt p0) {
+		if (zwischenp1p2(p0) || vorp1(p0) || hinterp2(p0))
+			return true;
+		return false;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj.getClass().equals(this.getClass())) {
+			
+		}
+		return false;
+	}
+
 }
--- Punkt.java
+++ Punkt.java
@@ -7,45 +7,53 @@ public class Punkt {
 		x = new BigDecimal(pX);
 		y = new BigDecimal(pY);
 	}
-	
+
 	public Punkt(BigDecimal pX, BigDecimal pY) {
-		x=pX;
-		y=pY;
+		x = pX;
+		y = pY;
 	}
 
 	public String toString() {
 		return "(" + x.toPlainString() + "," + y.toPlainString() + ")";
 	}
-	
+
 	public BigDecimal getX() {
 		return x;
 	}
-	
+
 	public BigDecimal getY() {
 		return y;
 	}
-	
+
 	public BigDecimal abstand(Punkt other) {
-		BigDecimal dX =x.subtract(other.getX());
+		BigDecimal dX = x.subtract(other.getX());
 		BigDecimal dY = y.subtract(other.getY());
 		return BigDecimalUtility.sqrt(dX.multiply(dX).add(dY.multiply(dY)));
 	}
-	
+
 	public boolean equals(Object obj) {
-		if(obj instanceof Punkt) {
+		if (obj instanceof Punkt) {
 			Punkt p1 = (Punkt) (obj);
-			return (BigDecimalUtility.equalValues(x, p1.getX())&&BigDecimalUtility.equalValues(y, p1.getY()));
+			return (BigDecimalUtility.equalValues(x, p1.getX()) && BigDecimalUtility.equalValues(y, p1.getY()));
 		}
 		return false;
 	}
-	
+
 	public static void main(String[] args) {
-		Punkt p1 = new Punkt(15d, 21d);
+		Punkt p1 = new Punkt(15d, 20d);
 		System.out.println(p1.toString());
-		Punkt p2 = new Punkt(15d, 21d);
+		Punkt p2 = new Punkt(20d, 25d);
 		System.out.println(p2.toString());
 		System.out.println(p1.abstand(p2).toPlainString());
 		System.out.println(p1.equals(p2));
+		Gerade g = new Gerade(p2, p1);
+		System.out.println(g.toString());
+		System.out.println("" + g.enthaelt(new Punkt(14d, 19d)) + g.enthaelt(p1) + g.enthaelt(new Punkt(16d, 21d))
+				+ g.enthaelt(p2) + g.enthaelt(new Punkt(21d, 26d)));
+		System.out.println(g.enthaelt(new Punkt(17d, 21d)) + "" + g.enthaelt(new Punkt(21d, 21d)));
+		System.out.println(p1.toString() + g.enthaelt(p1) + g.zwischenp1p2(p1));
+		System.out.println(g.vorp1(p1) + "");
+		System.out.println("" + g.hinterp2(p1));
 	}
-	
+
 }
